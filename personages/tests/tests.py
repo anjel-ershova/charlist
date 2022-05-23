5593ec95055319343b9d0a9e865de470d5807ac2
Anjel Ershova <ershova.a@skbkontur.ru>
2022-05-24 00:49:08 +0300

    Tests fixed

--- personages/tests/tests.py
+++ personages/tests/tests.py
@@ -3,6 +3,7 @@ import unittest
 from django.test import TestCase
 from django.test.utils import setup_test_environment, override_settings
 
+from myauth.models import CharlistUser
 from personages.models import Personage
 # Анжель, на досуге добавь тесты, как тут: https://developer.mozilla.org/ru/docs/Learn/Server-side/Django/Testing
 
@@ -13,22 +14,33 @@ class PersonageTest(TestCase):
         personages_count = Personage.objects.count()
         self.assertEqual(personages_count, 1)
 
-    @unittest.skip("demonstrating skipping")
     @override_settings(DEBUG=True)
     def test_main_login_required(self):
         response = self.client.get('/all_personages')
-        self.assertEqual(302, response.status_code)  # ошибка AssertionError: 302 != 301
+        # По умолчанию http клиент не проходит перенаправления. По этому проверяем так.
+        self.assertRedirects(response, '/all_personages/', status_code=301, target_status_code=302)
 
-        self.client.login(username='user_can_CRUD_personage', password='passwordCRUD')  # force login
-        response = self.client.get('/all_personages')
+        # Создадим пользователя
+        u = CharlistUser(username="admin1", email="admin@localhost", is_active=True)
+        # Пароль надо задавать через метод. Т.к. в таблице хранятся хеши паролей.
+        u.set_password("1q2w3e")
+        u.save()
+
+        response = self.client.login(username='admin1', password="1q2w3e")  # force login
+        self.assertTrue(response)
+
+        # В отличие от response = self.client.get('/all_personages')
+        # тут нам нет смысла вручную обрабатывать редиректы. По этому просто ставим follow=True
+        # Т.е. проходить все редиректы автоматически.
+        response = self.client.get('/all_personages', follow=True)
         self.assertEqual(200, response.status_code)
-        print(response.content.decode())
-        self.assertFalse(response.context['all_personages'].is_anonymous)
-        # self.assertIn('View all personages', response.content.decode()) # ошибка AssertionError: 'View all personages' not found in ''
+
+        # print(response.content.decode())
 
     @override_settings(DEBUG=True)
     def test_get_personage_detail(self):
+        self.client.login(username='user_can_CRUD_personage', password='passwordCRUD')
         response = self.client.get('/personage/details/1/')
         self.assertEqual(200, response.status_code)
-        self.assertIn('name: First', response.content.decode())
+        self.assertIn('First', response.content.decode())
 
